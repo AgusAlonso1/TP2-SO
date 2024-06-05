5ef914e8e34545f23277d3cb859c4af55bb9d33a
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-05 11:55:48 -0300

    commit updating of kernel.c

--- x64barebones/Kernel/KernelFiles/kernel.c
+++ x64barebones/Kernel/KernelFiles/kernel.c
@@ -9,6 +9,7 @@
 #include <interruptions.h>
 #include <scheduler.h>
 #include <test_util.h>
+#include <timer.h>
 
 extern uint8_t text;
 extern uint8_t rodata;
@@ -22,16 +23,16 @@ static const uint64_t PageSize = 0x1000;
 static void * const sampleCodeModuleAddress = (void*)0x400000;
 static void * const sampleDataModuleAddress = (void*)0x500000;
 
+
 typedef int (*EntryPoint)();
 
 
-void clearBSS(void * bssAddress, uint64_t bssSize)
-{
+
+void clearBSS(void * bssAddress, uint64_t bssSize) {
 	memset(bssAddress, 0, bssSize);
 }
 
-void * getStackBase()
-{
+void * getStackBase() {
 	return (void*)(
 		(uint64_t)&endOfKernel
 		+ PageSize * 8				//The size of the stack itself, 32KiB
@@ -39,36 +40,38 @@ void * getStackBase()
 	);
 }
 
-void * initializeKernelBinary()
-{
-
-	void * moduleAddresses[] = {
-		sampleCodeModuleAddress,
-		sampleDataModuleAddress
-	};
-
-	loadModules(&endOfKernelBinary, moduleAddresses);
-
-	clearBSS(&bss, &endOfKernel - &bss);
+void * initializeKernelBinary() {
+	void * moduleAddresses[] = {sampleCodeModuleAddress,sampleDataModuleAddress };
+    loadModules(&endOfKernelBinary, moduleAddresses);
+    clearBSS(&bss, &endOfKernel - &bss);
 
 	return getStackBase();
 }
 
+int idle(int argc, char **argv){
+    char * args[] = {"2", "shell", NULL};   // el 2 es el argc
+    uint32_t currentPid = getCurrentPid();
+    createProcessFromSched("shell", 1, LEVEL4, (Function) sampleCodeModuleAddress, args, currentPid);
+
+    while (1){
+        _hlt();
+    }
+
+    return 0;
+}
+
+
 int main() {
 	_cli(); // Disable interruptions
-    loadIDT();
 	createMemoryManager((void * ) MEMORY_MANAGER_FIRST_ADDRESS, pow2(MAX_EXP));
+	createScheduler();
+//CREATE SEMAFOROS
+//CREATE PIPEMASTER
 
-	//createScheduler();
-	_sti(); // Enable interruptions
+	char * args[] = {"2", "idle", NULL};
+    createProcessFromSched("idle", 1, LEVEL3, (Function) &idle, args, IDLE);
 
-	char * argv1[] = {"10", NULL};
-	char * argv2[] = {"33554432", NULL};
-	//test_mm(1, argv1);
-	test_mm(1, argv2);
-	test_processes(1, argv1);
-
-	((EntryPoint)sampleCodeModuleAddress)();
-
-	return 0;
+    loadIDT();
+    _sti(); // Enable interruptions
+    return 0;
 }
