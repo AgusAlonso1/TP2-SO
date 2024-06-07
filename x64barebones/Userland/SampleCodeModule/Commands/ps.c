f8571dfb1f15e1dea69f776732ce10e0120dfa6b
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-07 19:30:20 -0300

    commit fixing bug in ps

--- x64barebones/Userland/SampleCodeModule/Commands/ps.c
+++ x64barebones/Userland/SampleCodeModule/Commands/ps.c
@@ -10,24 +10,13 @@ int ps(int argc, char** argv) {
     }
 
 
-
     for (int i = 0; i < processCopyList->length; i++) {
         ProcessCopy *processCopy = &processCopyList->processCopyList[i];
-        printf(" NAME: %s PID: %d PRIORITY: %d STATE: %s ", processCopy->name, processCopy->pid, (int) processCopy->priority, states[processCopy->state]);
-        printf("STACK: %p BASE: %p POSITION: %s \n", processCopy->stack, processCopy->basePointer, processCopy->position == 1 ? "foreground" : "background");
-        call_free(processCopyList->processCopyList[i].name);
+        printf(" Name: %s PID: %d Priority: %d State: %s ", processCopyList->processCopyList[i].name, processCopy->pid, (int) processCopy->priority, states[processCopy->state]);
+        printf("Stack Pointer: %p Base Pointer: %p Position: %s \n", processCopy->stack, processCopy->basePointer, processCopy->position == 1 ? "Foreground" : "Background");
     }
-    call_free(processCopyList->processCopyList);
-    call_free(processCopyList);
 
-    /*
-    printf("name \t id \t priority \t state \t stack \t base \t foreground \n");
+    call_free_process_copy(processCopyList);
 
-
-    for (uint64_t i = 0; i < length; i++) {
-        ProcessCopy * processCopy = getProcessIndex(processCopyList, i);
-        printf("%s \t %d \t %d \t %s \t %p \t %p \t %s \n", processCopy->name, processCopy->pid, (int) processCopy->priority, states[processCopy->state], processCopy->stack, processCopy->basePointer, processCopy->position == 1 ? "fg" : "bg");
-    }
-     */
     return 0;
 }
