c5321460697fd2caa7206867a08598205437af71
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-07 12:09:19 -0300

    commit fixing of bugs

--- x64barebones/Userland/SampleCodeModule/include/syscallFunctions.h
+++ x64barebones/Userland/SampleCodeModule/include/syscallFunctions.h
@@ -3,25 +3,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
-
-typedef int (*Function)(int, char **);
-
-#define RUNNING 0
-#define READY 1
-#define BLOCKED 2
-#define ZOMBIE 3
-
-typedef struct ProcessCopy {
-    uint32_t pid;
-    char * name;
-    uint64_t priority;
-    uint64_t state;
-    void * stack;
-    void * basePointer;
-    char position;
-} ProcessCopy;
-
-typedef struct ProcessCopyListCDT * ProcessCopyListADT;
+#include <globals.h>
 
 
 typedef enum {WRITE=0, DELETE, ENTER, RELATIVE_ENTER, TAB}actionOfCursor;
