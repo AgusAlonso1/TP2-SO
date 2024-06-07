0def6e8e166b3203fee023ca90cd0e2dce19e1f4
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-07 19:30:54 -0300

    commit updating loop function so that it births a child

--- x64barebones/Userland/SampleCodeModule/Commands/loop.c
+++ x64barebones/Userland/SampleCodeModule/Commands/loop.c
@@ -1,4 +1,5 @@
 #include <commands.h>
+static int loop2(int argc, char** argv);
 
 int loop(int argc, char** argv) {
     if(argc < 1){
@@ -8,6 +9,15 @@ int loop(int argc, char** argv) {
     uint32_t pid = call_get_pid();
     while(1){
         printf("Hola soy el process: %d\n", pid);
-        call_sleep(3000);
+        uint32_t childPid = call_create_process("childPid", 0, (Function) loop2, argv, pid);
+        call_sleep(ms);
+    }
+}
+
+static int loop2(int argc, char** argv){
+    while(1){
+        printf("Hola soy un hijo\n");
+
+        call_sleep(10000);
     }
 }
