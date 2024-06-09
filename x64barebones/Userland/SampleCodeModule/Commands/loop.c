04bf9e7526d3c348ffad7da8d0db4e6ea6e519b2
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-09 20:16:29 -0300

    commit making loop sleep by seconds

--- x64barebones/Userland/SampleCodeModule/Commands/loop.c
+++ x64barebones/Userland/SampleCodeModule/Commands/loop.c
@@ -1,15 +1,14 @@
 #include <commands.h>
-static int loop2(int argc, char** argv);
 
 int loop(int argc, char** argv) {
     if(argc < 1){
         return -1;
     }
-    int ms = atoi(argv[0]);
+    int seconds = atoi(argv[0]);
     uint32_t pid = call_get_pid();
     while(1){
         printf("Hola soy el process: %d\n", pid);
-        call_sleep(ms);
+        call_sleep_seconds(seconds);
     }
     return 0;
 }
