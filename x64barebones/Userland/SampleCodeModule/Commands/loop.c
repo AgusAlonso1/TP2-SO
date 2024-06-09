26e04f9d66a0a2089df325d4dfbe5cdc3f033774
valentinamartireta <vmartireta@itba.edu.ar>
2024-06-09 14:33:39 -0300

    commit fixing of bugs in commands functions

--- x64barebones/Userland/SampleCodeModule/Commands/loop.c
+++ x64barebones/Userland/SampleCodeModule/Commands/loop.c
@@ -9,15 +9,6 @@ int loop(int argc, char** argv) {
     uint32_t pid = call_get_pid();
     while(1){
         printf("Hola soy el process: %d\n", pid);
-      //  uint32_t childPid = call_create_process("childPid", 0, (Function) loop2, argv, pid);
         call_sleep(ms);
-    }
-}
-
-static int loop2(int argc, char** argv){
-    while(1){
-        printf("Hola soy un hijo\n");
-
-        call_sleep(10000);
     }
 }
