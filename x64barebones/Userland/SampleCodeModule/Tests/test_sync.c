a8de7ef8e6bb34d3ee8c1f75796878b96563d600
TomxBC <tobecerra@itba.edu.ar>
2024-06-10 21:10:41 -0300

    Add test_sync to userland.

--- x64barebones/Userland/SampleCodeModule/Tests/test_sync.c
+++ x64barebones/Userland/SampleCodeModule/Tests/test_sync.c
@@ -18,11 +18,9 @@ void slowInc(int64_t *p, int64_t inc) {
 }
 
 int my_process_inc(int argc, char *argv[]) {
-  uint32_t p = call_get_pid();
   uint64_t n;
   int8_t inc;
   int8_t use_sem;
-
 
   if (argc != 3)
     return -1;
@@ -34,11 +32,6 @@ int my_process_inc(int argc, char *argv[]) {
   if ((use_sem = satoi(argv[2])) < 0)
     return -1;
 
-  if (use_sem)
-    if ((call_sem_open(1, SEM_ID)) < 0) {
-      printf("test_sync: ERROR opening semaphore\n");
-      return -1;
-    }
 
   uint64_t i;
   for (i = 0; i < n; i++) {
@@ -67,8 +60,14 @@ int test_sync(int argc, char *argv[]) { //{n, use_sem, 0}
 
   global = 0;
 
+   if (useSem)
+    if ((call_sem_open(1, SEM_ID)) < 0) {
+      printf("test_sync: ERROR opening semaphore\n");
+      return -1;
+    }
+
   uint64_t i;
-  int fileDescriptors[CANT_FILE_DESCRIPTORS] = {STDIN, STDOUT, STDERR};
+  int fileDescriptors[CANT_FILE_DESCRIPTORS] = {DEV_NULL, STDOUT, STDERR};
   for (i = 0; i < TOTAL_PAIR_PROCESSES; i++) {
     pids[i] = call_create_process_background("my_process_inc", &my_process_inc, argvInc , test_pid, fileDescriptors);
     pids[i + TOTAL_PAIR_PROCESSES] = call_create_process_background("my_process_dec", &my_process_inc ,argvDec, test_pid, fileDescriptors);
@@ -79,10 +78,11 @@ int test_sync(int argc, char *argv[]) { //{n, use_sem, 0}
     call_waitpid(pids[i + TOTAL_PAIR_PROCESSES]);
   }
 
-  if (useSem)
-		call_sem_close(SEM_ID);
 
   printf("Succes %d\n", global);
+
+  if (useSem)
+		call_sem_close(SEM_ID);
 
   return 0;
 }
